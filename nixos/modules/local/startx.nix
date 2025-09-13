588f7b081af86787bc0eedf4254e4cfd74a6371b
Milan Glacier <dev@milanglacier.com>
2025-09-13 02:06:30 -0400

    refactor(nixos/startx): Introduce user-level options to override system configuration.
    This change enables the `startx` module to function independently of a
    NixOS system configuration. To use it in such an environment, configure
    the user-level options, which will prevent Nix from searching for a
    system-level configuration.

--- nixos/modules/local/startx.nix
+++ nixos/modules/local/startx.nix
@@ -1,23 +1,44 @@
-{ pkgs, config, osConfig, lib, ... }:
-let
-    shell = if (osConfig != null) then osConfig.users.users.${config.home.username}.shell else pkgs.zsh;
-    systemWindowManager = if (osConfig != null) then osConfig.services.displayManager.defaultSession else null;
-    windowManager = if (systemWindowManager != null) then systemWindowManager else "none+i3";
-in {
-    programs.${shell.pname} = {
-        # Only startx if there is no DISPLAY and we are on the first
-        # virtual terminal
-        profileExtra = lib.mkOrder 1000 ''
-            [[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx
-        '';
+{ pkgs, config, osConfig, lib, ... }: {
+    options.milanglacier.${config.home.username} = {
+        shell = lib.mkOption {
+            type = lib.types.package;
+            default = pkgs.zsh;
+            description = "Shell package of the user";
+        };
+        windowManager = lib.mkOption {
+            type = lib.types.str;
+            default = "none+i3";
+            description = "Window Manager session of the user";
+        };
     };
 
-    home.file.".xinitrc".text = ''
+    config = let
+        sysShell = if (osConfig != null) then osConfig.users.users.${config.home.username}.shell else null;
+        sysSession = if (osConfig != null) then osConfig.services.displayManager.defaultSession else null;
+        cfg = config.milanglacier.${config.home.username};
+    in {
+        milanglacier.${config.home.username} = {
+            shell = lib.mkIf (sysShell != null) (lib.mkDefault sysShell);
+            windowManager = lib.mkIf (sysSession != null) (lib.mkDefault sysSession);
+        };
+
+        programs.${cfg.shell.pname} = {
+            # Only startx if there is no DISPLAY and we are on the first
+            # virtual terminal
+            profileExtra = lib.mkOrder 1000 ''
+            [[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx
+            '';
+        };
+
+        home.file.".xinitrc".text = let
+            windowManager = cfg.windowManager;
+        in ''
         export QT_SCALE_FACTOR=2
         # This is required for IM working in kitty
         export GLFW_IM_MODULE=ibus
         export LIBGL_ALWAYS_SOFTWARE=1
         # Extract the session name (e.g., "i3" from "none+i3") and execute it.
         exec ${lib.last (builtins.split "\\+" windowManager)}
-    '';
+        '';
+    };
 }
